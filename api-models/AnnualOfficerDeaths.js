7f081ddc9ebbed9c102430b1309c22b603cc0c0e
Peter Schmalfeldt <peter@manifestinteractive.com>
2021-05-09 06:29:53 -0400

    Updating ORM Schema to address MySQL import requirements

--- api-models/AnnualOfficerDeaths.js
+++ api-models/AnnualOfficerDeaths.js
@@ -1,67 +1,78 @@
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const AnnualOfficerDeaths = sequelize.define(
     'AnnualOfficerDeaths', {
       id: {
-        type: DataTypes.UUID,
+        type: DataTypes.INTEGER(15).UNSIGNED,
         primaryKey: true,
         allowNull: false,
+        autoIncrement: true
       },
       agency_id: {
-        type: DataTypes.UUID(15),
+        type: DataTypes.INTEGER(15).UNSIGNED,
+        allowNull: false
+      },
+      source_year: {
+        type: DataTypes.INTEGER(4),
         allowNull: false,
+        comment: 'XWK_6 - Year of merged data'
+      },
+      source_name: {
+        type: DataTypes.STRING(100),
+        comment: 'XWK_7 - Data Source Name'
       },
       group_code: {
         type: DataTypes.STRING(2),
-        comment: 'LEO_1 Group, LEOKA',
+        comment: 'LEO_1 Group, LEOKA'
       },
       total_population: {
-        type: DataTypes.DOUBLE(9),
-        comment: 'LEO_2 Total Population, LEOKA',
+        type: DataTypes.FLOAT(9),
+        comment: 'LEO_2 Total Population, LEOKA'
       },
       total_employees: {
-        type: DataTypes.DOUBLE(5),
-        comment: 'LEO_5 Total Employees, LEOKA',
+        type: DataTypes.FLOAT(5),
+        comment: 'LEO_5 Total Employees, LEOKA'
       },
       total_officers: {
         type: DataTypes.INTEGER(3),
-        comment: 'LEO_6 Total Officers, LEOKA',
+        comment: 'LEO_6 Total Officers, LEOKA'
       },
       male_officers: {
-        type: DataTypes.DOUBLE(5),
-        comment: 'LEO_3 Male Officers, LEOKA',
+        type: DataTypes.FLOAT(5),
+        comment: 'LEO_3 Male Officers, LEOKA'
       },
       female_officers: {
-        type: DataTypes.DOUBLE(5),
-        comment: 'LEO_4 Female Officers, LEOKA',
+        type: DataTypes.FLOAT(5),
+        comment: 'LEO_4 Female Officers, LEOKA'
       },
       officer_rate_per_10k: {
         type: DataTypes.INTEGER(3),
-        comment: 'LEO_7 Officer Rate per 10,000, LEOKA',
+        comment: 'LEO_7 Officer Rate per 10,000, LEOKA'
       },
       months_reported_assaults_with_injury: {
-        type: DataTypes.DOUBLE(3),
-        comment: 'LEO_10 Number of Months Reported, Assaults with Injury, LEOKA',
+        type: DataTypes.FLOAT(3),
+        comment: 'LEO_10 Number of Months Reported, Assaults with Injury, LEOKA'
       },
       months_reported_assaults_without_injury: {
-        type: DataTypes.DOUBLE(3),
-        comment: 'LEO_11 Number of Months Reported, Assaults without Injury, LEOKA',
+        type: DataTypes.FLOAT(3),
+        comment: 'LEO_11 Number of Months Reported, Assaults without Injury, LEOKA'
       },
       annual_assaults_with_injury: {
         type: DataTypes.FLOAT(7),
-        comment: 'LEO_8 Annual Number of Assaults With Injury, LEOKA',
+        comment: 'LEO_8 Annual Number of Assaults With Injury, LEOKA'
       },
       annual_assaults_without_injury: {
         type: DataTypes.FLOAT(7),
-        comment: 'LEO_9 Annual Number of Assaults Without Injury, LEOKA',
+        comment: 'LEO_9 Annual Number of Assaults Without Injury, LEOKA'
       },
       annual_officers_killed_felony: {
         type: DataTypes.FLOAT(3),
-        comment: 'LEO_12 Annual Number of Officers Killed, Felony, LEOKA',
+        comment: 'LEO_12 Annual Number of Officers Killed, Felony, LEOKA'
       },
       annual_officers_killed_accident: {
         type: DataTypes.FLOAT(3),
-        comment: 'LEO_13 Annual Number of Officers Killed, Accident, LEOKA',
-      },
+        comment: 'LEO_13 Annual Number of Officers Killed, Accident, LEOKA'
+      }
     }, {
       timestamps: true,
       paranoid: true,
@@ -73,8 +84,19 @@ module.exports = (sequelize, DataTypes) => {
       collate: 'utf8_unicode_ci',
       charset: 'utf8mb4',
       comment: 'Officers Assaulted and Killed Annual Death Counts',
-    },
-  );
+      indexes: [
+        {
+          unique: true,
+          name: 'annual_officer_deaths_unique_ix',
+          fields: ['agency_id', 'source_year']
+        },
+        {
+          name: 'annual_officer_deaths_year_ix',
+          fields: ['source_year']
+        }
+      ]
+    }
+  )
 
-  return AnnualOfficerDeaths;
-};
+  return AnnualOfficerDeaths
+}
--- api-models/Gunshots.js
+++ api-models/Gunshots.js
@@ -1,51 +1,54 @@
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Gunshots = sequelize.define(
     'Gunshots', {
       id: {
-        type: DataTypes.UUID,
+        type: DataTypes.INTEGER(15).UNSIGNED,
         primaryKey: true,
         allowNull: false,
+        autoIncrement: true
       },
       agency_id: {
-        type: DataTypes.UUID(15),
+        type: DataTypes.INTEGER(15).UNSIGNED,
+        allowNull: false
+      },
+      source_year: {
+        type: DataTypes.INTEGER(4),
         allowNull: false,
+        comment: 'XWK_6 - Year of merged data'
       },
-      ori9: {
-        type: DataTypes.STRING(9),
-        comment: 'VIC_1 ORI9, Vice',
-      },
-      agency: {
+      source_name: {
         type: DataTypes.STRING(100),
-        comment: 'VIC_2 Agency, Vice',
+        comment: 'XWK_7 - Data Source Name'
       },
       shots_fired: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_3 Total Number of Shots Fired Count, Vice',
+        comment: 'VIC_3 Total Number of Shots Fired Count, Vice'
       },
       fatal_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_4 Fatal Shootings Count, Vice',
+        comment: 'VIC_4 Fatal Shootings Count, Vice'
       },
       possible_fatal_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_5 Unknown if Fatal Shootings Count, Vice',
+        comment: 'VIC_5 Unknown if Fatal Shootings Count, Vice'
       },
       non_fatal_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_6 Nonfatal Shootings Count, Vice',
+        comment: 'VIC_6 Nonfatal Shootings Count, Vice'
       },
       unarmed_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_7 Subject Unarmed Shootings Count, Vice',
+        comment: 'VIC_7 Subject Unarmed Shootings Count, Vice'
       },
       possible_armed_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_8 Unknown if Subject Armed Shootings Count, Vice',
+        comment: 'VIC_8 Unknown if Subject Armed Shootings Count, Vice'
       },
       armed_shootings: {
         type: DataTypes.INTEGER(9),
-        comment: 'VIC_9 Subject Armed Shootings Count, Vice',
-      },
+        comment: 'VIC_9 Subject Armed Shootings Count, Vice'
+      }
     }, {
       timestamps: true,
       paranoid: true,
@@ -54,9 +57,22 @@ module.exports = (sequelize, DataTypes) => {
       plural: 'gunshots',
       tableName: 'gunshots',
       engine: 'InnoDB',
+      collate: 'utf8_unicode_ci',
+      charset: 'utf8mb4',
       comment: 'Gunshot Measures',
-    },
-  );
+      indexes: [
+        {
+          unique: true,
+          name: 'gunshots_unique_ix',
+          fields: ['agency_id', 'source_year']
+        },
+        {
+          name: 'gunshots_year_ix',
+          fields: ['source_year']
+        }
+      ]
+    }
+  )
 
-  return Gunshots;
-};
+  return Gunshots
+}
